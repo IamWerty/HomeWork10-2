cbc8a5bfb721ce3bac93fd63902783fe9dbc3c3d
Григорій . <mcwertyge@gmail.com>
2025-07-02 15:13:25 +0300

    add features

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,27 @@
 ﻿using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
+
+StringBuilder report = new StringBuilder();
+report.AppendLine($"Звіт подій\nДата: {DateTime.Now.ToString("dd.MM.yyyy")}\n-----------------------------");
+
+bool isWhile = true;
+
+while (isWhile)
+{
+    Console.Write("Подія: ");
+    string input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        isWhile = false;
+        break;
+    }
+    report.AppendLine("- " + input);
+}
+
+report.AppendLine("-----------------------------");
+report.AppendLine("Кінець звіту");
+
+Console.WriteLine("\nЗгенерований звіт:\n");
+Console.WriteLine(report.ToString());
